facca07067415a395aedf10c3a55051b32bf2ff7
Ali Haddadi <ali.haddadi154@gmail.com>
2024-09-26 15:22:37 +0330

    fixed name bug

--- TelegramBot.py
+++ TelegramBot.py
@@ -219,7 +219,13 @@ def signin(message):
             username, password,
             0)
             if sfa.signedIn:
-                user_db_id = database.add({"username":username, "password":encrypt_password(password).decode('utf8').replace("'", '"'), "telid":user_id, "chatid":message.chat.id,"name":message.from_user.first_name + " " + message.from_user.last_name,"username":message.from_user.username, "autoReserve":False, "session":0, "days":[]})
+                name = ""
+                if message.from_user.first_name:
+                    if message.from_user.last_name:
+                        name += message.from_user.first_name + message.from_user.last_name
+                    else:
+                        name += message.from_user.first_name
+                user_db_id = database.add({"username":username, "password":encrypt_password(password).decode('utf8').replace("'", '"'), "telid":user_id, "chatid":message.chat.id,"name":name,"username":message.from_user.username, "autoReserve":False, "session":0, "days":[]})
                 store_session(sfa.currentSession, user_db_id)
                 bot.send_message(message.chat.id, "شما وارد شدید")
                 if markup.keyboard[0][0]["text"] == "🔓 ورود":
---
+++ sendCustumeMessage.py
@@ -0,0 +1,40 @@
+from pysondb import db
+import requests
+import json
+from src.api import *
+from cryptography.fernet import Fernet
+import telebot
+import environmets
+import platform
+
+bot = telebot.TeleBot(environmets.tellApiKey, parse_mode=None)
+
+# Initialize the database
+db_path = './db/users.json' if platform.system() == 'Windows' else f'{environmets.dbPath}/users.json'
+
+database = db.getDb(db_path)  # Replace with your actual database file
+
+key_path = 'secret.key' if platform.system() == 'Windows' else f'{environmets.keyPath}/secret.key'
+
+def load_key():
+    return open(key_path, "rb").read()
+
+# Function to encrypt a password
+def encrypt_password(password):
+    key = load_key()
+    f = Fernet(key)
+    encrypted_password = f.encrypt(password.encode())
+    return encrypted_password
+
+# Function to decrypt an encrypted password
+def decrypt_password(encrypted_password):
+    key = load_key()
+    f = Fernet(key)
+    decrypted_password = f.decrypt(encrypted_password.encode('utf-8')).decode()
+    return decrypted_password
+
+users = database.getAll()
+message = input("enter your message:")
+
+for user in users:
+    bot.send_message(user["telid"],message)
